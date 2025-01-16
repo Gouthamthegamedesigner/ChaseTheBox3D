dd3daafc17e2da644aa410eeee0061c0c7360a84
Gouthamthegamedesigner <gouthamkrishna1206@gmail.com>
2025-01-16 22:44:25 +0530

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField]
+    private float playerSpeed = 2.0f;
+
+    private float gravityValue = -9.81f;
+    private Player playerInput;
+
+    private void Awake () {
+        playerInput = new Player();
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable(){
+        playerInput.Enable();
+    }
+
+    private void OnDisable(){
+        playerInput.Disable();
+    }
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movementInput = playerInput.PlayerMain.Move.ReadValue<Vector2>();
+
+        Vector3 move = new Vector3(movementInput.x, 0f, movementInput.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
