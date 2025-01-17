b46631465fbf61b818cfbd9fa3545ad956d1b1c1
Gouthamthegamedesigner <gouthamkrishna1206@gmail.com>
2025-01-17 10:58:57 +0530

    Movement updation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,43 +15,48 @@ public class PlayerController : MonoBehaviour
     private float gravityValue = -9.81f;
     private Player playerInput;
 
-    private void Awake () {
+    private void Awake()
+    {
         playerInput = new Player();
         controller = GetComponent<CharacterController>();
     }
 
-    private void OnEnable(){
+    private void OnEnable()
+    {
         playerInput.Enable();
     }
 
-    private void OnDisable(){
-        playerInput.Disable();
-    }
-
-    private void Start()
+    private void OnDisable()
     {
-        
+        playerInput.Disable();
     }
 
     void Update()
     {
         groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        if (groundedPlayer)
         {
-            playerVelocity.y = 0f;
+            playerVelocity.y = 0f; // Reset vertical velocity when grounded
+        }
+        else
+        {
+            playerVelocity.y += gravityValue * Time.deltaTime; // Apply gravity when not grounded
         }
 
+        // Get movement input
         Vector2 movementInput = playerInput.PlayerMain.Move.ReadValue<Vector2>();
-
         Vector3 move = new Vector3(movementInput.x, 0f, movementInput.y);
+
+        // Apply movement
         controller.Move(move * Time.deltaTime * playerSpeed);
 
+        // Rotate player to face movement direction
         if (move != Vector3.zero)
         {
             gameObject.transform.forward = move;
         }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
+        // Apply gravity
         controller.Move(playerVelocity * Time.deltaTime);
     }
 }
