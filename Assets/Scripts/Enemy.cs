8ab7fe734e97d25dda00f58244f8d8de59a4c12d
Gouthamthegamedesigner <gouthamkrishna1206@gmail.com>
2025-01-20 17:26:31 +0530

    Setting up minimap

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+   [SerializeField] private float _speed = 9;
+
+   private Transform _target;
+
+   void Awake()
+   {
+        _target = FindObjectOfType<PlayerMovement>().transform;
+   }
+
+    void Update()
+    {
+         Vector3 targetPosition = new Vector3(_target.position.x, transform.position.y, _target.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, _speed * Time.deltaTime);
+
+        // Rotate towards the player
+        Vector3 direction = targetPosition - transform.position;
+        if (direction != Vector3.zero)
+        {
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, _speed * Time.deltaTime);
+        }
+    }
+
+    
+
+}
